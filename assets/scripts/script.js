8e45af2fed137f33a24ed788f9103370a2db0420
Joe Christianson <jcfargond@gmail.com>
2022-03-30 16:58:43 -0500

    changed css to final styling

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -11,27 +11,31 @@ let schedule = JSON.parse(localStorage.getItem("schedule"))||{};
 
 for (i=start;i<end;i++){
     let bgColor;
+    let color=""
     if (i<currentHour){
-        bgColor = "bg-light"
+        bgColor = "past";
     }
     else if(i===currentHour){
-        bgColor = "bg-danger"
+        bgColor = "present"
     }
-    else bgColor = "bg-success";
+    else {
+        bgColor = "future";
+    }
     const row = $("<tr>");
     const hour = $("<td>");
     hour.addClass("col-2");
     hour.text(i===12?12:i%12);
     hour.data("hour",i)
     const item = $("<td>");
-    item.addClass("col-9 "+bgColor);
+    item.addClass("col-9 "+bgColor+" item ");
     const input = $("<textarea>")
+    input.addClass(color)
     input.addClass("col-12")
     input.attr("data-hour",i)
     input.val(schedule[i])
     item.append(input)
     const saveBtn = $("<td>");
-    saveBtn.text("#128274")
+    saveBtn.text("💾")
     saveBtn.data("hour",i)
     saveBtn.addClass("col-1 bg-primary text-white"+" custom-save-btn");
     
